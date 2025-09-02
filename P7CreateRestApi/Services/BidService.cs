362e3fc25e494c12d994db6cee06f4ebcbb04309
Julien LEROY <julien.leroy.work@gmail.com>
2025-09-02 22:01:22 +0200

    Clean Service + use Automapper

--- P7CreateRestApi/Services/BidService.cs
+++ P7CreateRestApi/Services/BidService.cs
@@ -1,4 +1,5 @@
-﻿using P7CreateRestApi.Domain;
+﻿using AutoMapper;
+using P7CreateRestApi.Domain;
 using P7CreateRestApi.Dto;
 using P7CreateRestApi.Repositories;
 
@@ -16,59 +17,39 @@ namespace P7CreateRestApi.Services
     public class BidService : IBidService
     {
         private readonly IRepository<Bid> _repository;
+        private readonly IMapper _mapper;
 
-        public BidService(IRepository<Bid> repository)
+        public BidService(IRepository<Bid> repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
-        public async Task<BidDto> CreateAsync(BidDto bidModel)
+        public async Task<List<BidDto>> GetAllAsync()
         {
-            var bid = new Bid
-            {
-                Account = bidModel.Account,
-                Type = bidModel.Type,
-                BidQuantity = bidModel.BidQuantity
-            };
-
-            var created = await _repository.AddAsync(bid);
-
-            return new BidDto
-            {
-                Id = created.Id,
-                Account = created.Account,
-                Type = created.Type,
-                BidQuantity = created.BidQuantity
-            };
-
+            return await _repository.GetAllAsync()
+                                    .ContinueWith(task => task.Result
+                                    .Select(bid => _mapper.Map<BidDto>(bid))
+                                    .ToList());
         }
 
         public async Task<BidDto?> GetByIdAsync(int id)
         {
             var bid = await _repository.GetByIdAsync(id);
+
             if (bid == null)
-            {
                 return null;
-            }
-            return new BidDto
-            {
-                Id = bid.Id,
-                Account = bid.Account,
-                Type = bid.Type,
-                BidQuantity = bid.BidQuantity
-            };
+            else
+                return _mapper.Map<BidDto>(bid);
         }
 
-        public async Task<bool> DeleteAsync(int id)
+        public async Task<BidDto> CreateAsync(BidDto bidDto)
         {
-            var existingBid = await _repository.GetByIdAsync(id);
-            if (existingBid != null)
-            {
-                _repository.Remove(existingBid);
-                await _repository.SaveChangesAsync();
-                return true;
-            }
-            return false;
+            var bid = _mapper.Map<Bid>(bidDto);
+
+            var created = await _repository.AddAsync(bid);
+
+            return _mapper.Map<BidDto>(created);
         }
 
         public async Task<BidDto?> UpdateAsync(int id, BidDto bidDto)
@@ -83,30 +64,38 @@ namespace P7CreateRestApi.Services
             existingBid.Account = bidDto.Account;
             existingBid.Type = bidDto.Type;
             existingBid.BidQuantity = bidDto.BidQuantity;
+            existingBid.CreationDate = bidDto.CreationDate;
+            existingBid.BidDate = bidDto.BidDate;
+            existingBid.BidSecurity = bidDto.BidSecurity;
+            existingBid.BidStatus = bidDto.BidStatus;
+            existingBid.AskQuantity = bidDto.AskQuantity;
+            existingBid.Benchmark = bidDto.Benchmark;
+            existingBid.Commentary = bidDto.Commentary;
+            existingBid.Trader = bidDto.Trader;
+            existingBid.Book = bidDto.Book;
+            existingBid.CreationName = bidDto.CreationName;
+            existingBid.RevisionName = bidDto.RevisionName;
+            existingBid.RevisionDate = bidDto.RevisionDate;
+            existingBid.DealName = bidDto.DealName;
+            existingBid.DealType = bidDto.DealType;
+            existingBid.SourceListId = bidDto.SourceListId;
+            existingBid.Side = bidDto.Side;
 
             var updated = await _repository.UpdateAsync(existingBid);
 
-            return new BidDto
-            {
-                Id = updated.Id,
-                Account = updated.Account,
-                Type = updated.Type,
-                BidQuantity = updated.BidQuantity
-            };
+            return _mapper.Map<BidDto>(updated);
         }
 
-        public async Task<List<BidDto>> GetAllAsync()
+        public async Task<bool> DeleteAsync(int id)
         {
-            return await _repository.GetAllAsync()
-                .ContinueWith(task => task.Result
-                    .Select(bid => new BidDto
-                    {
-                        Id = bid.Id,
-                        Account = bid.Account,
-                        Type = bid.Type,
-                        BidQuantity = bid.BidQuantity
-                    })
-                    .ToList());
+            var existingBid = await _repository.GetByIdAsync(id);
+            if (existingBid != null)
+            {
+                _repository.Remove(existingBid);
+                await _repository.SaveChangesAsync();
+                return true;
+            }
+            return false;
         }
     }
 }
